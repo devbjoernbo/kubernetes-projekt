f9628213f44842f009c1bc3082def080a6c85c6a
Nicolai Christensen <nicolai.bjornbo@hotmail.com>
2020-02-07 00:24:14 +0100

    changed-console-log

--- api-server/server/server.js
+++ api-server/server/server.js
@@ -66,7 +66,7 @@ app.post("/values", async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
-    return res.status(422).send("Index too high");
+    return res.status(422).send("Index too high a number");
   }
 
   redisUtils.redisClient.hset("values", index, "Nothing yet!");
