3eb7ad6f1197a5183152d79b5e852220ad1beaea
Nicolai Christensen <nicolai.bjornbo@hotmail.com>
2020-02-08 17:46:33 +0100

    new-images-commit

--- web-client/app/services/apollo_client/apollo_client.js
+++ web-client/app/services/apollo_client/apollo_client.js
@@ -1,6 +1,6 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from "apollo-boost";
 
 export default new ApolloClient({
-  uri: 'http://localhost:4000',
-  credentials: 'include'
+  uri: "http://localhost:4000/api",
+  credentials: "include"
 });
